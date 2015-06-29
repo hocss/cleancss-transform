669059dd377f0109e5b04e361288166cf1cc5def
Matt Styles <matt@veryfizzyjelly.com>
2015-06-29 17:30:52 +0100

    #add clean process and test

--- spec/transform.js
+++ spec/transform.js
@@ -5,32 +5,12 @@ var pkg = require( '../package.json' )
 var test = require( 'tape' )
 var Transform = require( '../dist/transform' )
 
-var fixtureCompiled = 'body {\n  -webkit-transform: scale( 2 );\n          transform: scale( 2 );\n}\n'
-
-test( 'Transform stream produces prefixed css', function( t ) {
+test( 'Transform stream produces minified css', function( t ) {
     t.plan( 1 )
 
     var transform = new Transform()
     var outPath = path.join( os.tmpdir(), pkg.name + '-' + Math.random() )
-    var expectedOutput = 'body { -webkit-transform: scale( 2 ); transform: scale( 2 ); }\n'
-
-    fs.createReadStream( __dirname + '/fixture.css' )
-        .pipe( transform )
-        .pipe( fs.createWriteStream( outPath )
-            .on( 'close', function() {
-                t.equal( fs.readFileSync( outPath, { encoding: 'utf8' } ), expectedOutput, 'Transform prefixes css' )
-            }))
-})
-
-test( 'Browsers to prefix for should be passed through to autoprefixer', function( t ) {
-    t.plan( 1 )
-
-    var transform = new Transform({
-        browsers: [ 'chrome 40' ]
-    })
-    
-    var outPath = path.join( os.tmpdir(), pkg.name + '-' + Math.random() )
-    var expectedOutput = 'body { transform: scale( 2 ); }\n'
+    var expectedOutput = 'body{transform:scale(2)}'
 
     fs.createReadStream( __dirname + '/fixture.css' )
         .pipe( transform )
